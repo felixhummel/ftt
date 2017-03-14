502c43122b82118c6ff8d0d92877b87701299d63
Felix Hummel <felix@felixhummel.de>
2017-03-14 23:33:52 +0100

    epic timezone aware view! \o/

--- ftt/urls.py
+++ ftt/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.hello, name='hello'),
+    url(r'^$', views.HealthView.as_view(), name='hello'),
 ]
--- ftt/views.py
+++ ftt/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.utils import timezone
+from django.views.generic import TemplateView
 
 
-def hello(request):
-    return HttpResponse('Hi', content_type='text/plain')
+class HealthView(TemplateView):
+    template_name = 'ftt/health.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
